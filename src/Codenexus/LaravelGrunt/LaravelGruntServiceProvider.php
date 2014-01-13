e91e42a25291b0259c0d342b67e41728ca0b8c56
Jeremy Schaffer <jeremys@codenexus.org>
2014-01-12 20:59:39 -0800

    Initial commit

---
+++ src/Codenexus/LaravelGrunt/LaravelGruntServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Codenexus\LaravelGrunt;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelGruntServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('codenexus/laravel-grunt');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
